e32ee4fcaeb7a65c3f744b121c73a9ac54262898
medghoul <ghoulhamza06@gmail.com>
2024-11-02 20:45:17 +0100

    Add comments

--- src/server.js
+++ src/server.js
@@ -4,15 +4,19 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
+// Middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
   console.log("Morgan enabled...");
 }
 
+// Load environment variables
 dotenv.config({
   path: "./config.env",
 });
 
+
+// Connect to MongoDB
 mongoose.connect(process.env.DB_URI).then((conn) => {
   console.log(`Connected to MongoDB: ${conn.connection.host}`);
   })
@@ -21,10 +25,13 @@ mongoose.connect(process.env.DB_URI).then((conn) => {
     process.exit(1);
   });
 
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Start server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
