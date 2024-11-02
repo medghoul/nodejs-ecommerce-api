9464b757c18ed66d2955e0d1dc2476993a20ff7c
medghoul <ghoulhamza06@gmail.com>
2024-11-02 20:14:06 +0100

    Add mongodb configuration

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-    console.log('Morgan enabled...');
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log("Morgan enabled...");
 }
 
 dotenv.config({
-    path: './config.env'
+  path: "./config.env",
+});
+
+mongoose.connect(process.env.DB_URI).then(() => {
+    console.log("Connected to MongoDB...");
+  }).catch((err) => {
+    console.log("Error connecting to MongoDB...", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
