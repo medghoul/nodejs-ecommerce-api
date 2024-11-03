b1e02a7a71e72f26caa2a9f68239b5734ff944f5
Mohamed Hamza Ghoul <ghoulhamza06@gmail.com>
2024-11-03 14:46:34 +0100

    Update category model

--- src/api/v1/models/category.model.js
+++ src/api/v1/models/category.model.js
@@ -1,11 +1,25 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-const CategorySchema = new Schema({
+const CategorySchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
-    }
-});
+      type: String,
+      required: true,
+      unique: true,
+      minlength: [3, "Name must be at least 3 characters long"],
+      maxlength: [32, "Name must be at most 32 characters long"],
+    },
+    // Slug is a unique identifier for the category
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-export default model('Category', CategorySchema);
+export default model("Category", CategorySchema);
