851abc47b712661363487424914388f064880f5e
medghoul <ghoulhamza06@gmail.com>
2024-11-03 12:36:42 +0100

    Modify config.js for environment settings

--- config/config.js
+++ config/config.js
@@ -1,9 +1,23 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import Logger from '../utils/logger.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, '../config.env') });
+
+Logger.info('Config:', {
+    message: {
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.PORT,
+        DB_URI: process.env.DB_URI
+    }
+});
 
 export default {
     NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT || 5000,
-    MONGO_URI: process.env.MONGO_URI,
+    MONGO_URI: process.env.DB_URI,
     API_VERSION: '/api/v1'
 };
